5e73ecb7d7a285f43b5ad31e7423d22ecc8f5e12
Justin A <32287615+jabercrombia@users.noreply.github.com>
2025-05-27 21:02:40 -0500

    tabs wrapper

--- app/job/[slug]/page.tsx
+++ app/job/[slug]/page.tsx
@@ -1,18 +1,14 @@
 import { getJobDescriptionEntries } from "@/lib/api";
 import TabsWrapper from "../../components/TabsWrapper";
+
 interface PageProps {
   params: {
     slug: string;
   };
 }
 
-export default async function PlatformPage({ params }: PageProps) {
-  const { slug } = params;
-  const entries = await getJobDescriptionEntries(slug);
+export default async function Page({ params }: PageProps) {
+  const entries = await getJobDescriptionEntries(params.slug);
 
-  return (
-    <div className="container mx-auto">
-      <TabsWrapper entries={entries} slug={slug} />
-    </div>
-  );
+  return <TabsWrapper entries={entries} slug={params.slug} />;
 }
